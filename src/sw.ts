b4a99974051889f5f9efe0d4bc5bc843a36df0a3
julianegoe <goersch.juliane@dpa.com>
2022-09-10 16:07:23 +0200

    trial pwa

--- src/sw.ts
+++ src/sw.ts
@@ -1,37 +1,7 @@
-import { registerRoute } from 'workbox-routing';
-import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
-import { setCacheNameDetails } from 'workbox-core';
-import { addRoute } from 'workbox-precaching';
-import { ExpirationPlugin } from 'workbox-expiration';
-
-addRoute(self.__WB_MANIFEST);
-
-setCacheNameDetails({ prefix: 'feedback-hub' });
-
-// Cache api Calls
-const baseURL = `${import.meta.env.VITE_BASE_URL}/`;
-const apiRouteRegEx = baseURL.replace('\\.', '\\\\.');
-
-registerRoute(
-  new RegExp(apiRouteRegEx.concat('cuisines*')),
-  new StaleWhileRevalidate({ cacheName: 'cuisines' }),
-);
-registerRoute(
-  new RegExp(apiRouteRegEx.concat('leckerlog*')),
-  new StaleWhileRevalidate({ cacheName: 'leckerlog' }),
-);
-
-// Cache portal icons
-registerRoute(
-  // eslint-disable-next-line prefer-regex-literals
-  new RegExp('https://www\\.google\\.com/s2/favicons*'),
-  new CacheFirst({
-    cacheName: 'favicons',
-    plugins: [
-      new ExpirationPlugin({
-        // cache for 10 min
-        maxAgeSeconds: 10 * 60,
-      }),
-    ],
-  }),
-);
+import { precacheAndRoute } from 'workbox-precaching'
+declare let self: ServiceWorkerGlobalScope
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting()
+})
+// self.__WB_MANIFEST is default injection point
+precacheAndRoute(self.__WB_MANIFEST)
