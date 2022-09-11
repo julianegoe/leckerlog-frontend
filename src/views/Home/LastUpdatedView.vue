86650532174a9fa2dcbb06e06293e92a54d463a7
julianegoe <goersch.juliane@dpa.com>
2022-09-11 23:11:46 +0200

    delete in list view

--- src/views/Home/LastUpdatedView.vue
+++ src/views/Home/LastUpdatedView.vue
@@ -2,19 +2,27 @@
 import { useContentStore } from '../../store/content';
 import FoodCard from '../../components/FoodCard.vue';
 import AppEmptyState from '../../components/AppEmptyState.vue';
+import { useApi } from '../../composables/useApi';
 
 defineProps<{
     status: 'LOADING' | 'IDLE' | 'SUCCESS' | 'ERROR';
 }>()
 
 const content = useContentStore();
+const api = useApi()
+
+const handleDelete = async (id: number) => {
+  await api.deleteFoodOrdered(id);
+  const leckerlog = await api.getLeckerlog();
+  content.setLeckerlogs(leckerlog);
+};
 
 </script>
 <template>
     <div>
         <div class="flex flex-col gap-2">
             <template v-for="(food, index) in content.getLastUpdated" :key="`${index}-${food.name}`">
-                <FoodCard :menu-item="food.name" :rating="food.rating" :file-name="food.image_path"
+                <FoodCard @delete="handleDelete(food.food_id)" :menu-item="food.name" :rating="food.rating" :file-name="food.image_path"
                     :comment="food.comment" :date="food.ordered_at" :restaurant-name="food.restaurant_name" />
             </template>
         </div>
