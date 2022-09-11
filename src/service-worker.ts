d237e88db52d3f2c5d1135d653455a20abf9bcbf
julianegoe <goersch.juliane@dpa.com>
2022-09-11 11:44:54 +0200

    new strategy

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,39 +1,24 @@
 import { registerRoute } from 'workbox-routing';
-import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { NetworkFirst } from 'workbox-strategies';
 import { setCacheNameDetails } from 'workbox-core';
 import { addRoute } from 'workbox-precaching';
-import { ExpirationPlugin } from 'workbox-expiration';
 
 declare let self: any
 
 addRoute(self.__WB_MANIFEST);
 
-setCacheNameDetails({ prefix: 'leckerlog' });
-
-// Cache api Calls
-const baseURL = `${import.meta.env.VITE_BASE_URL}/`;
+const baseURL = `${import.meta.env.VITE_BASE_API_URL}/`;
 const apiRouteRegEx = baseURL.replace('\\.', '\\\\.');
 
+
+setCacheNameDetails({ prefix: 'leckerlog' });
+
 registerRoute(
-  new RegExp(apiRouteRegEx.concat('cuisines*')),
-  new StaleWhileRevalidate({ cacheName: 'cuisines' }),
-);
-registerRoute(
-  new RegExp(apiRouteRegEx.concat('leckerlog*')),
-  new StaleWhileRevalidate({ cacheName: 'leckerlog' }),
+    new RegExp(apiRouteRegEx.concat('cuisines*')),
+    new NetworkFirst()
 );
 
-// Cache portal icons
 registerRoute(
-  // eslint-disable-next-line prefer-regex-literals
-  new RegExp('https://www\\.google\\.com/s2/favicons*'),
-  new CacheFirst({
-    cacheName: 'favicons',
-    plugins: [
-      new ExpirationPlugin({
-        // cache for 10 min
-        maxAgeSeconds: 10 * 60,
-      }),
-    ],
-  }),
+    new RegExp(apiRouteRegEx.concat('leckerlog*')),
+    new NetworkFirst()
 );
