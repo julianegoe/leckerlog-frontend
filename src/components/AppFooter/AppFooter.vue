108d2c5e55c0bda9e4ddc39edfa57bfa62c39722
julianegoe <goersch.juliane@dpa.com>
2022-09-16 18:38:46 +0200

    content above bar

--- src/components/AppFooter/AppFooter.vue
+++ src/components/AppFooter/AppFooter.vue
@@ -7,36 +7,37 @@ import List from '../../assets/icons/list.svg';
 const isUploading = ref(false);
 </script>
 <template>
-  <footer id="bottom-nav" class="fixed bottom-0 w-full h-16 bg-gray-200 flex justify-start items-center  border border-1 border-black">
-    <router-link :to="{name: 'AddFood'}">
-      <AddButton :is-uploading="isUploading" />
-    </router-link>
-    <router-link :to="{name: 'Home'}">
-      <button :disabled="isUploading"
-        class="w-24 h-16 border-r border-black cursor-pointer grow-1 flex flex-col items-center justify-center hover:bg-gray-400"
-        :class="{
-          'disabled:text-gray-500 disabled:cursor-default' : isUploading,
-        }">
-        <List />
-        <div class="text-xs">Liste</div>
-      </button>
-    </router-link>
-    <router-link :to="{name: 'Categories'}">
-      <button :disabled="isUploading"
-        class="w-24 h-16 border-r border-black cursor-pointer grow-1 flex flex-col items-center justify-center hover:bg-gray-400"
-        :class="{
-          'disabled:text-gray-500 disabled:cursor-default' : isUploading,
-        }">
-        <Squares />
-        <div class="text-xs">Kategorien</div>
-      </button>
-    </router-link>
-  </footer>
+  <div id="bottom-nav">
+    <footer
+      class="fixed bottom-0 w-full h-16 bg-gray-200 flex justify-start items-center  border border-1 border-black">
+      <router-link :to="{name: 'AddFood'}">
+        <AddButton :is-uploading="isUploading" />
+      </router-link>
+      <router-link :to="{name: 'Home'}">
+        <button :disabled="isUploading"
+          class="w-24 h-16 border-r border-black cursor-pointer grow-1 flex flex-col items-center justify-center hover:bg-gray-400"
+          :class="{
+            'disabled:text-gray-500 disabled:cursor-default' : isUploading,
+          }">
+          <List />
+          <div class="text-xs">Liste</div>
+        </button>
+      </router-link>
+      <router-link :to="{name: 'Categories'}">
+        <button :disabled="isUploading"
+          class="w-24 h-16 border-r border-black cursor-pointer grow-1 flex flex-col items-center justify-center hover:bg-gray-400"
+          :class="{
+            'disabled:text-gray-500 disabled:cursor-default' : isUploading,
+          }">
+          <Squares />
+          <div class="text-xs">Kategorien</div>
+        </button>
+      </router-link>
+    </footer>
+  </div>
 </template>
-<style scroped>
-
+<style>
 #bottom-nav {
   padding-bottom: env(safe-area-inset-bottom);
 }
-
 </style>
