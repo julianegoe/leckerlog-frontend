3988dd9a32965abb26400f21b69374887736189f
julianegoe <goersch.juliane@dpa.com>
2022-09-16 18:55:44 +0200

    position nav bar

--- src/components/AppFooter/AppFooter.vue
+++ src/components/AppFooter/AppFooter.vue
@@ -35,7 +35,7 @@ const isUploading = ref(false);
 </template>
 <style scroped>
   #bottom-nav {
-    height: env(safe-area-inset-bottom) + 64px;
+    height: env(safe-area-inset-bottom) * 2;
     padding-bottom: env(safe-area-inset-bottom);
   }
 </style>
