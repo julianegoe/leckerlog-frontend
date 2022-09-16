bcdfd7e970712aa51566ab9b331faa3c7d739cbc
julianegoe <goersch.juliane@dpa.com>
2022-09-16 18:29:09 +0200

    content above bar

--- src/components/AppFooter/AppFooter.vue
+++ src/components/AppFooter/AppFooter.vue
@@ -7,7 +7,7 @@ import List from '../../assets/icons/list.svg';
 const isUploading = ref(false);
 </script>
 <template>
-  <footer id="bottom-nav" class="sticky bottom-0 w-full h-16 bg-gray-200 flex justify-start items-center  border border-1 border-black">
+  <footer id="bottom-nav" class="fixed bottom-0 w-full h-16 bg-gray-200 flex justify-start items-center  border border-1 border-black">
     <router-link :to="{name: 'AddFood'}">
       <AddButton :is-uploading="isUploading" />
     </router-link>
