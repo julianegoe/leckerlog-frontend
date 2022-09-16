434844ecebb8d6e05cb2a5f71b28970c31373060
julianegoe <goersch.juliane@dpa.com>
2022-09-16 22:33:24 +0200

    add logo

--- vite.config.ts
+++ vite.config.ts
@@ -20,12 +20,12 @@ export default defineConfig({
       theme_color: '#ffffff',
       icons: [
         {
-          src: '/icon-512x512.png',
+          src: '/512.png',
           sizes: '512x512',
           type: 'image/png'
         },
         {
-          src: '/icon-192x192.png',
+          src: '/192.png',
           sizes: '192x192',
           type: 'image/png',
         }
