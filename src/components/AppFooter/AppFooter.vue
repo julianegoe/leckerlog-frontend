4ac769b370a06fe5682ad2e6063b0343caf85843
julianegoe <goersch.juliane@dpa.com>
2022-09-16 23:21:56 +0200

    WIP nav bar overhaul

--- src/components/AppFooter/AppFooter.vue
+++ src/components/AppFooter/AppFooter.vue
@@ -8,38 +8,42 @@ const isUploading = ref(false);
 </script>
 <template>
   <footer id="bottom-nav"
-    class="fixed bottom-0 w-full bg-gray-200 flex justify-start items-center border border-1 border-black">
+    class="fixed bottom-0 w-full bg-blue-200 flex justify-start items-center ">
     <router-link :to="{name: 'AddFood'}">
-      <button :disabled="isUploading"
-        id="add-button"
-        class="group w-24 border-r border-black cursor-pointer grow-1 flex flex-col items-center justify-center hover:bg-gray-400 pt-2 pb-4"
+      <button :disabled="isUploading" id="list-button"
+        class="group w-24 cursor-pointer grow-1 flex flex-col items-center justify-centerpy-2"
         :class="{
           'disabled:text-gray-500 disabled:cursor-default' : isUploading,
         }">
-        <Plus class="group-hover:transition-transform group-hover:scale-125 group-hover:ease-in" />
+        <Plus class="group-hover:transition-transform group-hover:scale-150 group-hover:ease-in" />
         <div class="text-xs">Hinzufügen</div>
       </button>
     </router-link>
     <router-link :to="{name: 'Home'}">
       <button :disabled="isUploading" id="list-button"
-        class="group w-24 border-r border-black cursor-pointer grow-1 flex flex-col items-center justify-center hover:bg-gray-400 pt-2 pb-4"
+        class="group w-24 cursor-pointer grow-1 flex flex-col items-center justify-centerpy-2"
         :class="{
           'disabled:text-gray-500 disabled:cursor-default' : isUploading,
         }">
-        <List class="group-hover:transition-transform group-hover:scale-125 group-hover:ease-in" />
+        <List class="group-hover:transition-transform group-hover:scale-150 group-hover:ease-in" />
         <div class="text-xs">Liste</div>
       </button>
     </router-link>
     <router-link :to="{name: 'Categories'}">
       <button :disabled="isUploading" id="categories-button"
-        class="group w-24 border-r border-black cursor-pointer grow-1 flex flex-col items-center justify-center hover:bg-gray-400 pt-2 pb-4"
+        class="group w-24  cursor-pointer grow-1 flex flex-col items-center justify-center py-2"
         :class="{
           'disabled:text-gray-500 disabled:cursor-default' : isUploading,
         }">
-        <Squares class="group-hover:transition-transform group-hover:scale-125 group-hover:ease-in" />
+        <Squares class="group-hover:transition-transform group-hover:scale-150 group-hover:ease-in" />
         <div class="text-xs">Kategorien</div>
       </button>
     </router-link>
   </footer>
 </template>
+<style scoped>
+  #add-button, #list-button, #categoried-button {
+    padding-bottom: env(safe-area-inset-bottom);
+  }
+</style>
 
