472c66153a09ba20f538dcffcf4c9461b3b2c68b
julianegoe <goersch.juliane@dpa.com>
2022-09-19 21:11:44 +0200

    fixed layout

--- src/components/FoodCard.vue
+++ src/components/FoodCard.vue
@@ -24,8 +24,8 @@ const localeDateString = computed(() => {
 
 </script>
 <template>
-  <div @click="$router.push({ name: 'Details', params: { foodId } })"  class="flex flex-shrink-1 justify-between border-2 bg-white border-black shadow-brutal rounded-md">
-    <div class="flex flex-col gap-2 p-2">
+  <div @click="$router.push({ name: 'Details', params: { foodId } })"  class="grid grid-cols-3 border-2 bg-white border-black shadow-brutal rounded-md">
+    <div class="col-span-2 flex flex-col gap-2 p-2">
       <p class="text-xs">{{ localeDateString }}</p>
       <h1 class="text-xl font-bold">{{ menuItem }}</h1>
       <p class="text-xs">{{ restaurantName }}</p>
@@ -35,7 +35,7 @@ const localeDateString = computed(() => {
       <p class="line-clamp-2">"{{ comment }}"</p>
     </div>
     <img v-if="result.status.value === 'SUCCESS'"
-      class="rounded-r-md aspect-[3/4] max-h-48 object-cover object-center border-l-2 border-black"
+      class="col-span-1 rounded-r-md h-full object-cover object-center border-l-2 border-black"
       :src="result.imageUrl.value" :alt="menuItem">
     <div v-else-if="result.status.value === 'LOADING'"
       class="rounded-r-md aspect-[3/4] h-48 border-l-2 border-black bg-gray-200 animate-pulse"></div>
--- src/composables/useFileDownload.ts
+++ src/composables/useFileDownload.ts
@@ -1,5 +1,5 @@
 import { ref as storageRef, getDownloadURL, getStorage } from "firebase/storage";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 
 type UploadStatus = 'LOADING' | 'IDLE' | 'SUCCESS' | 'ERROR';
 type ImageSize = 'thumbnail' | 'hero' | 'fallback';
@@ -8,28 +8,40 @@ export const useFileDownload = (fileName: string | null, imageSize: ImageSize) =
   const status = ref<UploadStatus>('IDLE');
   const imageUrl = ref<string>('https://via.placeholder.com/300x400');
 
-  const sizeLookUp = new Map().set('thumbnail', '200x200').set('hero', '1500x1500').set('fallback', '500x500')
+  const sizeLookUp = new Map([
+    ['thumbnail', '200x200'],
+    ['hero', '1500x1500'],
+    ['fallback', '200x200'],
+  ]);
   const size = ref(sizeLookUp.get(imageSize));
   const fallback = ref(sizeLookUp.get('fallback'));
+
+  const downloadUrl = computed(() => {
+    if (fileName !== null) {
+      return `${fileName.split('.')[0]}_${size.value}.${fileName.split('.')[1]}`
+    } return '';
+  })
   onMounted(() => {
     status.value = 'LOADING';
     const storage = getStorage();
-    if (fileName !== null) {
-      getDownloadURL(storageRef(storage, `${fileName.split('.')[0]}_${size.value}.${fileName.split('.')[1]}`))
+    if (downloadUrl) {
+      getDownloadURL(storageRef(storage, downloadUrl.value))
         .then((url) => {
           imageUrl.value = url;
           status.value = 'SUCCESS';
         })
         .catch((error) => {
           if (error.code === 'storage/object-not-found') {
-            getDownloadURL(storageRef(storage, `${fileName.split('.')[0]}_${fallback.value}.${fileName.split('.')[1]}`))
-              .then((url) => {
-                imageUrl.value = url;
-                status.value = 'SUCCESS';
-              }).catch((error) => {
-                console.log(error)
-                status.value = 'ERROR';
-              })
+            if (fileName !== null) {
+              getDownloadURL(storageRef(storage, `${fileName.split('.')[0]}_${fallback.value}.${fileName.split('.')[1]}`))
+                .then((url) => {
+                  imageUrl.value = url;
+                  status.value = 'SUCCESS';
+                }).catch((error) => {
+                  console.log(error)
+                  status.value = 'ERROR';
+                })
+            }
           } else { console.log(error) }
         });
     }
