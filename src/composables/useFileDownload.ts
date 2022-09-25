6d0f617bb65225fb65d8d6942a58a9cbbaf662ff
julianegoe <goersch.juliane@dpa.com>
2022-09-25 20:01:34 +0200

    changed default image size

--- src/composables/useFileDownload.ts
+++ src/composables/useFileDownload.ts
@@ -6,19 +6,15 @@ type ImageSize = 'thumbnail' | 'hero' | 'fallback';
 
 export const useFileDownload = (fileName: string | null, imageSize: ImageSize) => {
   const status = ref<UploadStatus>('IDLE');
-  const imageUrl = ref<string>('https://via.placeholder.com/300x400');
 
   const sizeLookUp = new Map([
     ['thumbnail', '500x500'],
     ['hero', '1500x1500'],
-    ['fallback', '200x200'],
   ]);
-  const size = ref(sizeLookUp.get(imageSize));
-  const fallback = ref(sizeLookUp.get('fallback'));
 
   const imagePath = computed(() => {
     if (fileName !== null) {
-      return `${fileName.split('.')[0]}_${size.value}.${fileName.split('.')[1]}`
+      return `${fileName.split('.')[0]}_${sizeLookUp.get(imageSize)}.${fileName.split('.')[1]}`
     } return '';
   });
 
@@ -29,7 +25,7 @@ export const useFileDownload = (fileName: string | null, imageSize: ImageSize) =
     } catch (error: any) {
       if (error.code === 'storage/object-not-found') {
         if (fileName !== null) {
-          return 'https://via.placeholder.com/500'
+          return 'https://via.placeholder.com/300x400'
         }
       }
     }
