a095364ed4cf6fa36a4babee72baeba91eb8a8e5
julianegoe <goersch.juliane@dpa.com>
2022-09-25 23:33:52 +0200

    image layout fixed

--- src/components/FoodCard.vue
+++ src/components/FoodCard.vue
@@ -31,16 +31,16 @@ const localeDateString = computed(() => {
 <template>
   <div @click="$router.push({ name: 'Details', params: { foodId } })"
     class="grid grid-cols-3 border-2 bg-white border-black shadow-brutal rounded-md">
-    <div class="col-span-2 flex flex-col gap-2 p-2">
+    <div class="h-48 col-span-2 flex flex-col gap-2 p-2">
       <p class="text-xs">{{ localeDateString }}</p>
-      <h1 class="text-xl font-bold">{{ menuItem }}</h1>
-      <p class="text-xs">{{ restaurantName }}</p>
+      <h1 class="text-lg font-bold">{{ menuItem }}</h1>
+      <p class="text-xs line-clamp-1">{{ restaurantName }}</p>
       <div class="flex gap-1">
         <StarIcon fill="#8affdc" class="h-6 w-6" v-for="n in rating" :key="`${n}-star-rating`" />
       </div>
       <p class="line-clamp-2">"{{ comment }}"</p>
     </div>
-      <img class="col-span-1" v-if="url" :src="url" :alt="menuItem">
+      <img class="h-48 col-span-1 object-cover object-center" v-if="url" :src="url" :alt="menuItem">
     <div v-else-if="!url" class="rounded-r-md col-span-1 h-48 border-l-2 border-black bg-gray-200 animate-pulse"></div>
   </div>
 
